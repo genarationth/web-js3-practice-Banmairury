0175175bcc287d11857b7ac863d0c34e616bf743
Teerapong <banmairuruy@gmail.com>
2023-03-13 16:24:00 +0700

    answer

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = (a,b) => console.log(a+b);;
+add(7,2);
+
+const subtraact = (a,b) => console.log(a-b);
+subtraact(7,2);
+
+const multiply = (a,b) => console.log(a*b);
+multiply(7,2);
+
+const divide = (a,b) => console.log(a/b);
+divide(7,2);
+
+const inform = (firstName,location,hobby) => console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+hobby);
+
+inform('Teerapong','Suphanburi','rc car')
